3057eff0e54dabbfb4b40166bfb00941b634bc01
Eugene <snegur.1998@gmail.com>
2023-04-12 02:15:13 +0300

    Added the html, js and gitignore files

--- dz-2.4/index.js
+++ dz-2.4/index.js
@@ -1,5 +1,5 @@
 let firstNumber = prompt('Select your first digit', '');
 let secondNumber = prompt('Select your second digit', '');
 let thirdNumber = prompt('Select your third digit', '');
-const meanNumber = (Number(firstNumber)+Number(secondNumber)+Number(thirdNumber))/3;
-alert(`Your arithmetic mean is ${meanNumber}`);
+const averageNumber = (Number(firstNumber)+Number(secondNumber)+Number(thirdNumber))/3;
+alert(`Your arithmetic mean is ${averageNumber}`);
