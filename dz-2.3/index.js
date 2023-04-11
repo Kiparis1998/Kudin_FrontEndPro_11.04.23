339179b114462ac05ede0ab701f6f6fe3aa1e291
Eugene <snegur.1998@gmail.com>
2023-04-12 01:24:03 +0300

    Added the html and gitignore files

---
+++ dz-2.3/index.js
@@ -0,0 +1,3 @@
+let hours = prompt('How many hours do you want to choose?', '');
+let seconds = Number(hours) * 3600;
+alert(`Wow! There are ${seconds} seconds in the amount of hours you've chosen recently!`);
