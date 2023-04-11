8f8d9da78e62180b52e66a43f91dd4898a32440b
Eugene <snegur.1998@gmail.com>
2023-04-12 01:01:15 +0300

    Added html and gitignore files

---
+++ dz-2.2/index.js
@@ -0,0 +1,6 @@
+let firstNumber = prompt('Input your first number', '2');
+let secondNumber = prompt('Input your second number', '2');
+alert(`${firstNumber}+${secondNumber}=${Number(firstNumber)+Number(secondNumber)}
+${firstNumber}-${secondNumber}=${Number(firstNumber)-Number(secondNumber)}
+${firstNumber}*${secondNumber}=${Number(firstNumber)*Number(secondNumber)}
+${firstNumber}/${secondNumber}=${Number(firstNumber)/Number(secondNumber)}`);
