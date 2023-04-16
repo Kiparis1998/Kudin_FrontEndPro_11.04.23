449487a9b1af2f17ac957129e8377566c953d08f
Eugene <snegur.1998@gmail.com>
2023-04-16 13:53:11 +0300

    Added the js file

--- if-else construction/index.js
+++ if-else construction/index.js
@@ -5,12 +5,12 @@ const sportPreference = prompt('What kind of sport are you keen on?', 'tennis');
 if(dateOfBirth && city && sportPreference) {
     alert(`Your date of birth - ${dateOfBirth}
 ${city === 'London' || city === 'Kyiv' || city === 'Washington' ? `You live in the capital of ${city}` : `You live in ${city}`}
-${sportPreference === 'bodybuilder' ? 'Wow, do you wanna be like Arnold Schwarzenegger?' : 
-        sportPreference === 'football' ? 'Wow, do you wanna be like Lionel Messi?' : 
+${sportPreference === 'bodybuilder' ? 'Wow, do you wanna be like Arnold Schwarzenegger?' :
+        sportPreference === 'football' ? 'Wow, do you wanna be like Lionel Messi?' :
             sportPreference === 'tennis' ? 'Wow, do you wanna be like Rafael Nadal?' :
                 'Sorry, I dont know any champions in this field of sport'} `);
 } else {
-    switch (dateOfBirth === null || city === null || sportPreference === null) {
+    switch (true) {
         case dateOfBirth === null && city !== null && sportPreference !== null:
             alert(`It's a pity you didn't point your date of birth
 ${city === 'London' || city === 'Kyiv' || city === 'Washington' ? `You live in the capital of ${city}` : `You live in ${city}`}
