f2b1e9d7cf5d9316bf736978c17faf6d9a7ce3e7
Eugene <snegur.1998@gmail.com>
2023-05-03 22:14:50 +0300

    Added js file

--- removeElementFunc/index.js
+++ removeElementFunc/index.js
@@ -1,12 +1,12 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
 removeElement = (arr, elem) => {
-    if (!array.includes(elem)) {
+    if (!arr.includes(elem)) {
         return `${elem} is not found`;
     }
-    const index = array.indexOf(elem);
-    array.splice(index, 1);
-    return array;
+    const index = arr.indexOf(elem);
+    arr.splice(index, 1);
+    return arr;
 }
 
 const result = removeElement(array, 5);
