b9ed089b3f1ead29287af7c2d6fd378586fcddfc
Eugene <snegur.1998@gmail.com>
2023-07-20 15:46:54 +0300

    Added varioes files

--- form/gulpfile.js
+++ form/gulpfile.js
@@ -1,40 +1,153 @@
+'use strict'
+
+const {src, dest} = require('gulp');
 const gulp = require('gulp');
-const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const cssbeautify = require('gulp-cssbeautify');
+const removeComments = require('gulp-strip-css-comments');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
+const rigger = require('gulp-rigger');
 const uglify = require('gulp-uglify');
-const cleanCSS = require('gulp-clean-css');
-const clean = require('gulp-clean');
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const panini = require('panini');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
 const browserSync = require('browser-sync').create();
 
-gulp.task('concatAndUglifyPlugins', () => {
-    return gulp.src('app/js/*.js')
-        .pipe(concat('concat.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+/* Paths */
+const srcPath = 'src/';
+const distPath = 'dist/';
+
+const path = {
+    build: {
+        html: distPath,
+        css: distPath + 'assets/css',
+        js: distPath + 'assets/js',
+        images: distPath + 'assets/images',
+        fonts: distPath + 'assets/fonts',
+    },
+    src: {
+        html: srcPath + '*.html',
+        css: srcPath + 'assets/css/*.css',
+        js: srcPath + 'assets/js/*.js',
+        images: srcPath + 'assets/images/**/*.{jpeg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+        fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
+    },
+    watch: {
+        html: srcPath + '**/*.html',
+        css: srcPath + 'assets/css/**/*.css',
+        js: srcPath + 'assets/js/**/*.js',
+        images: srcPath + 'assets/images/**/*.{jpeg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+        fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
+    },
+    clean: './' + distPath
+}
+
+gulp.task('server', () => {
+    browserSync.init({
+        server: {
+            baseDir: './' + distPath
+        }
+    });
 });
 
-gulp.task('minify-css', () => {
-    return gulp.src('app/css/*.css')
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest('dist'));
+gulp.task('html', () => {
+    panini.refresh();
+    return src(path.src.html, {base: srcPath})
+        .pipe(plumber())
+        .pipe(panini({
+            root: srcPath,
+            layouts: srcPath + 'tpl/layouts/',
+            partials: srcPath + 'tpl/partials/',
+        }))
+        .pipe(dest(path.build.html))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('css', () => {
+    return src(path.src.css, {base: srcPath + 'assets/css/'})
+        .pipe(plumber({
+            errorHandler: function (err) {
+                notify.onError({
+                    title: 'CSS Error',
+                    message: 'Error <%= error.message %>'
+                })(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(autoprefixer())
+        .pipe(cssbeautify())
+        .pipe(dest(path.build.css))
+        .pipe(cssnano({
+            zIndex: false,
+            discardComments: {
+                removeAll: true
+            }
+        }))
+        .pipe(removeComments())
+        .pipe(rename({
+            suffix: '.min',
+            extname: '.css'
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js', () => {
+    return src(path.src.js, {base: srcPath + 'assets/js/'})
+        .pipe(plumber({
+            errorHandler: function (err) {
+                notify.onError({
+                    title: 'JS Error',
+                    message: 'Error <%= error.message %>'
+                })(err);
+                this.emit('end');
+            }
+        }))
+        .pipe(rigger())
+        .pipe(dest(path.build.js))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min',
+            extname: '.js'
+        }))
+        .pipe(dest(path.build.js))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('images', () => {
+    return src(path.src.images, {base: srcPath + 'assets/images/'})
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 75, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(dest(path.build.images))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('fonts', () => {
+    return src(path.src.fonts, {base: srcPath + 'assets/fonts/'})
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('clean', () => {
-    return gulp.src('dist', {read: false})
-        .pipe(clean());
-});
-
-gulp.task('browserSyncServe', (cb) => {
-    browserSync.init({
-        server: {
-            baseDir: './app'
-        }
-    });
-    cb();
-});
-
-gulp.task('watchTask', () => {
-    gulp.watch('app/*.html').on('change', browserSync.reload);
-    gulp.watch(['app/**/*.css', 'app/**/*.js']).on('change', gulp.series('concatAndUglifyPlugins', 'minify-css', browserSync.reload));
+    return del(path.clean)
 })
 
-gulp.task('default', gulp.series('clean', gulp.parallel('concatAndUglifyPlugins', 'minify-css'), 'browserSyncServe', 'watchTask'));
+gulp.task('watchFiles', () => {
+    gulp.watch([path.watch.html, path.watch.css, path.watch.js, path.watch.images, path.watch.fonts])
+        .on('change', gulp.series(gulp.parallel('html', 'css', 'js', 'images', 'fonts'), browserSync.reload));
+})
+
+gulp.task('build', gulp.series('clean', gulp.parallel('html', 'css', 'js', 'images', 'fonts')));
+gulp.task('default', gulp.parallel('build', 'watchFiles', 'server'));
---
+++ form/src/assets/js/main.js
@@ -0,0 +1,78 @@
+const form = document.querySelector('.block-form');
+const name = document.querySelector('#name');
+const dateOfBirth = document.querySelector('#date-of-birth');
+const maleRadio = document.querySelector('#male');
+const femaleRadio = document.querySelector('#female');
+const city = document.querySelector('#city');
+const address = document.querySelector('#address');
+const frenchOption = document.querySelector('#french');
+const germanOption = document.querySelector('#german');
+const ukrainianOption = document.querySelector('#ukrainian');
+const englishOption = document.querySelector('#english');
+const email = document.querySelector('#email');
+const message = document.querySelector('.message');
+const table = document.querySelector('table');
+const returnButton = document.querySelector('.block-form_return-button');
+
+const arrOfTableTitles = [...document.querySelectorAll('th')];
+const arrOfLanguageOptions = [frenchOption, germanOption, ukrainianOption, englishOption];
+function validateData() {
+    const objectOfValues = {};
+    const checkedLanguages = arrOfLanguageOptions.filter(language => language.checked).map(language => language.value);
+    if (name.value.trim() !== '' && dateOfBirth.value.trim() !== '') {
+        objectOfValues['name and surname'] = name.value;
+        objectOfValues['date of birth'] = dateOfBirth.value;
+    } if (maleRadio.checked === true || femaleRadio.checked === true) {
+        maleRadio.checked === true ? objectOfValues['gender'] = maleRadio.value : objectOfValues['gender'] = femaleRadio.value;
+    } if (city.value !== 'empty') {
+        objectOfValues['city'] = city.value;
+    } if (address.value.trim() !== '') {
+        objectOfValues['address'] = address.value;
+    } if (checkedLanguages.length > 0) {
+        objectOfValues['languages'] = checkedLanguages.join(', ');
+    } if (email.value.trim() !== '' && (email.value.includes('@') && email.value.includes('.com'))) {
+        objectOfValues['email'] = email.value;
+    } if (Object.keys(objectOfValues).length === 7) {
+        return objectOfValues;
+    }
+    throw new Error('Invalid input');
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    try {
+        const result = validateData();
+        const row = document.createElement('tr');
+        arrOfTableTitles.forEach((cell) => {
+            Object.entries(result).forEach(([key, value]) => {
+                if (cell.innerText.toLowerCase() === key.toLowerCase()) {
+                    const content = document.createElement('td');
+                    if (key === 'city' || key === 'gender') {
+                        content.innerText = value.charAt(0).toUpperCase() + value.slice(1);
+                    } else {
+                        content.innerText = value;
+                    }
+                    row.append(content);
+                }
+            });
+        });
+        table.append(row);
+        form.classList.add('hidden');
+        table.classList.remove('hidden');
+        returnButton.classList.remove('hidden');
+    } catch (error) {
+        message.innerText = error.message;
+        form.classList.add('warning-border');
+        setTimeout(() => {
+            message.innerText = '';
+            form.classList.remove('warning-border');
+        }, 2000);
+    }
+});
+
+returnButton.addEventListener('click', () => {
+    form.classList.remove('hidden');
+    table.classList.add('hidden');
+    returnButton.classList.add('hidden');
+});
